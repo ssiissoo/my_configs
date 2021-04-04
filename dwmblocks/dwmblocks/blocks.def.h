7f9e7415056a4b8a10d7a8c43c3a698c4642615e
Simon Sochiera <ssochier@students.uni-mainz.de>
2021-04-04 10:50:09 +0200

    added dwmblocks

---
+++ dwmblocks/dwmblocks/blocks.def.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+	{"", "~/my_scripts/mic", 0, 11},
+	{"", "~/my_scripts/vol", 0, 10},
+	{"", "~/my_scripts/bat", 10, 0},
+	/*{" ", "echo $(printf '%03d\n' $(($(($(free -m | grep \"Mem:\" | awk '//{print $3}') * 100 )) / $(free -m | grep \"Mem:\" | awk '//{print $2}'))))", 30, 0},*/
+	{"", "date '+%a, %d.%m %H:%M:%S'", 1, 0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
