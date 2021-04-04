09a251a406ca742bb1c9435de52613129c28b3cb
Simon Sochiera <ssochier@students.uni-mainz.de>
2021-04-04 20:28:44 +0200

    modified dwmblocks

--- dwmblocks/dwmblocks/blocks.h
+++ dwmblocks/dwmblocks/blocks.h
@@ -10,4 +10,4 @@ static const Block blocks[] = {
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static unsigned int delimLen = 3;
