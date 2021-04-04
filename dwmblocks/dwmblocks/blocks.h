a713d897b0eabecab471dd5176ae81561edc7b63
Simon Sochiera <s.sochiera@web.de>
2021-04-04 19:59:37 +0200

    modified dwmblocks

--- dwmblocks/dwmblocks/blocks.h
+++ dwmblocks/dwmblocks/blocks.h
@@ -1,13 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
-	/*{"", "~/.script/news", 60, 14},
-	{"", "~/.script/task", 0, 11},
-	{"", "~/.script/event", 0, 11},
-	{"", "~/.script/inet", 5, 0},*/
-	{"", "~/.script/mic", 0, 11},
-	{"", "~/.script/vol", 0, 10},
-	{"", "~/.script/bat", 10, 0},
+	{"", "~/my_scripts/mic", 0, 11},
+	{"", "~/my_scripts/vol", 0, 10},
+	{"", "~/my_scripts/bat", 10, 0},
 	/*{" ", "echo $(printf '%03d\n' $(($(($(free -m | grep \"Mem:\" | awk '//{print $3}') * 100 )) / $(free -m | grep \"Mem:\" | awk '//{print $2}'))))", 30, 0},*/
 	{"", "date '+%a, %d.%m %H:%M:%S'", 1, 0},
 };
